da8d36e5aeb93d3ba523fc62cf803dc003f699fa
Gus Ballman <gus.ballman@wsu.edu>
2025-10-07 14:15:28 -0700

    Add files via upload

---
+++ Lab 12/Header.h
@@ -0,0 +1,20 @@
+/*
+
+
+*/
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void reverse_string(char string[], int start, int end);
+int sum_digits(int num, int sum);
+void find_path(char maze[8][8], int start_row, int start_col);
+
+#endif
---
+++ Lab 12/main.c
@@ -0,0 +1,38 @@
+/*
+
+
+*/
+
+#include "Header.h"
+
+int main(void)
+{
+	srand((unsigned int)time(NULL));
+	char string[10] = "Cpts 121";
+
+	// Task One:
+	reverse_string(string, 0, strlen(string) - 1);
+	printf("%s\n", string);
+
+	// Task Two:
+	int sum = 0, number = 0;
+
+	printf("Enter Number: ");
+	scanf("%d", &number);
+
+	sum = sum_digits(number, sum);
+	printf("%d\n", sum);
+
+	// Task Three:
+	char maze[8][8] = { "X  X XX",
+						"X XX X ",
+						"X     X",
+						"X XXX  ",
+						"    X  ",
+						"X X X X",
+						"  X   X",
+						"XXXXX  " };
+
+	find_path(maze, 0, 1);
+	return 0;
+}
