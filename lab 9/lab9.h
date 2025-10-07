1b8fe6e9a60cfe60f324261962c45bfce8ad1fb8
Gus Ballman <gus.ballman@wsu.edu>
2025-10-07 14:23:55 -0700

    Add files via upload

---
+++ lab 9/lab9.h
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+#ifndef LAB9_H
+#define LAB9_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<string.h>
+
+void string_reverse_array_notation(char str[], int size);
+char* my_strcpy(char destination[], const char source[]);
+char* my_strcat(char destination[], const char source[]);
+int my_strcmp(const char s1[], const char s2[]);
+int my_strlen(const char s[]);
+void string_reverse_pointer_notation(char* str);
+
+#endif
